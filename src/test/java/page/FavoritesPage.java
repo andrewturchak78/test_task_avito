66eb2b551625c3045deb7e817ceb5ae7e534a7ee
Andrey Turchak <andrewturchak78@mail.ru>
2023-08-28 13:49:02 +0300

    code of tests added

---
+++ src/test/java/page/FavoritesPage.java
@@ -0,0 +1,21 @@
+package page;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class FavoritesPage {
+    SelenideElement itemName = $x("//strong[.='Domain-Driven Design Distilled Vaughn Vernon']");
+    SelenideElement redHeartOfFavorite = $("[data-marker='toggle-favorite-icon']");
+    public String getItemName(){
+        return itemName.getText();
+    }
+    public void clickOnRedHeart(){
+        redHeartOfFavorite.click();
+    }
+    public FavoritesPage reloadPage(){
+        refresh();
+        return new FavoritesPage();
+    }
+}
---
+++ src/test/java/test/FavoritesTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import page.AddPage;
+import page.FavoritesPage;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class FavoritesTest {
+
+    @Test
+    void addInFavoriteTest(){
+        AddPage addPage = new AddPage();
+        FavoritesPage favPage = new FavoritesPage();
+        open("https://www.avito.ru/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363");
+        addPage.addInFavClick();
+        addPage.goToFavClick();
+        assertEquals("Domain-Driven Design Distilled Vaughn Vernon",favPage.getItemName());
+        favPage.clickOnRedHeart();
+        favPage.reloadPage();
+
+    }
+}
